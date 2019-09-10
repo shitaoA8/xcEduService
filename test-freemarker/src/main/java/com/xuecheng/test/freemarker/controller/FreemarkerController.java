b4cef351a0bb6d729b1db73a528c3ad9bb0a0b97
tao <553867585@qq.com>
2019-09-10 16:49:57 +0800

    "获取cms_config图片分类URL2"

---
+++ test-freemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
@@ -0,0 +1,22 @@
+package com.xuecheng.test.freemarker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@RequestMapping("/freemarker")
+@Controller
+public class FreemarkerController {
+
+    //测试1
+    @RequestMapping("/test")
+    public String test(Map<String, Object> map) {
+        System.out.println("+++++++++++++++++++++");
+        //map就是freemarker模板所使用的数据
+        map.put("name","王二小");
+        //返回freemarker模板的位置,基于resources/templates路径
+        return "test1";
+    }
+
+}
