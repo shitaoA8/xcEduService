0715f8069f86641de2b7402741b522e4ba5cd363
tao <553867585@qq.com>
2019-09-11 13:59:01 +0800

    "核对pom"

--- test-freemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
+++ test-freemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
@@ -10,13 +10,13 @@ import java.util.Map;
 public class FreemarkerController {
 
     //测试1
-    @RequestMapping("/test")
+    @RequestMapping("/test1")
     public String test(Map<String, Object> map) {
         System.out.println("+++++++++++++++++------");
         //map就是freemarker模板所使用的数据
         map.put("name","王二小");
         //返回freemarker模板的位置,基于resources/templates路径
-        return "test1.html";
+        return "test1";
     }
 
 }
